e64deff84ae2495ddc37dd2b369efefc404d9ec5
jesustissera <jesustissera@gmail.com>
2026-05-12 17:01:47 -0300

    refactor: agregar imagen de fondo

--- src/components/blocks/presentation/Presentation.tsx
+++ src/components/blocks/presentation/Presentation.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { MarkdownRenderer } from "@/components/ui/markdownRenderer/MarkdownRenderer";
 
 type Props = {
   translations: any;
@@ -7,27 +8,28 @@ type Props = {
 export const Presentation = ({ translations }: Props) => {
   
   return (
-    <section className="w-full py-20" >
+    <section className="w-full py-30 presentation relative overflow-hidden" >
+      <div className="absolute inset-0 bg-gradient-to-r from-black via-black/100 to-transparent" ></div>
       <div className="max-w-7xl mx-auto px-6">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center">
           <div data-aos="fade-right" data-aos-offset="300">
             <h1 className="text-4xl md:text-6xl font-bold text-gold">{translations.home.presentation.title}</h1>
 
-            <p className="mt-4 text-white leading-relaxed text-sm md:text-base">
-              { translations.home.presentation.description }
-            </p>
+            <div className="mt-4 text-white leading-relaxed text-sm md:text-base">
+              <MarkdownRenderer content={translations.home.presentation.description} />
+            </div>
           </div>
 
-          <div className="flex justify-center" data-aos="fade-left" data-aos-offset="300">
+          { /**<div className="flex justify-center" data-aos="fade-left" data-aos-offset="300">
             <div className="relative aspect-square w-full sm:w-[70%] overflow-hidden">
               <Image
                 src="/img/presentation.jpg"
                 alt="Sobre nosotros"
                 fill
                 className="object-cover"
-              />
+              /> 
             </div>
-          </div>
+          </div>*/}
         </div>
       </div>
     </section>
